17feb98e1320b575c03f7392cb713b8fe1b76e44
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-27 10:51:46 +0200

    setup project enviroment

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-purple-900">
+      <h1 className="text-5xl font-extrabold text-white">
+        Benvenuto in alx-project-0x02!
+      </h1>
+    </main>
+  );
+};
+
+export default HomePage;
