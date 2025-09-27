df88ba0e3185937c2e04d8779b8ae6ebb6194472
Saadia Nazih <saadia.nazih92@gmail.com>
2025-09-27 10:55:55 +0200

    added text on header file

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-full bg-gray-800 p-4 text-center text-white">
+      <h1 className="text-xl font-bold">
+        Il Mio Progetto
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
